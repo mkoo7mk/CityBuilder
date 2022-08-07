6f2be907e8c37a6c1cd573878772aaa54a35ab26
mkoo7mk <martinkokmet@seznam.cz>
2022-08-07 23:04:31 +0200

    Game/Menu mash

--- main/Classes/Renderer.py
+++ main/Classes/Renderer.py
@@ -3,20 +3,47 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
 
-def square():
-    glBegin(GL_QUADS)
-    glVertex2f(100, 100)
-    glVertex2f(200, 100)
-    glVertex2f(200, 200)
-    glVertex2f(100, 200)
+RESOLUTION = 500
+GUI_SCALE = 10
+zoom = 10  # How many lines of squares of screen will be visible
+
+
+def vertical_lines():
+    for x in range(zoom):
+        glVertex2f(RESOLUTION / zoom * x, RESOLUTION - (RESOLUTION / zoom * (zoom - 2)))
+        glVertex2f(RESOLUTION / zoom * x, RESOLUTION)
+
+
+def horizontal_lines():
+    for x in range(3, zoom):
+        glVertex2f(0, RESOLUTION / zoom * x)
+        glVertex2f(RESOLUTION, RESOLUTION / zoom * x)
+
+
+def menu_lines():
+    for x in range(1, 3):
+        glVertex2f(0, RESOLUTION / GUI_SCALE * x)
+        glVertex2f(RESOLUTION, RESOLUTION / GUI_SCALE * x)
+    for x in range(GUI_SCALE):
+        glVertex2f(RESOLUTION / zoom * x, RESOLUTION - (RESOLUTION / GUI_SCALE * (GUI_SCALE - 2)))
+        glVertex2f(RESOLUTION / zoom * x, 0)
+
+
+def mesh():
+    glBegin(GL_LINES)
+    glColor3f(3.0, 3.0, 3.0)
+    horizontal_lines()
+    vertical_lines()
+    glColor3f(0.0, 3.0, 1.0)
+    menu_lines()
     glEnd()
 
 
 def iterate():
-    glViewport(0, 0, 500, 500)
+    glViewport(0, 0, RESOLUTION, RESOLUTION)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+    glOrtho(0.0, RESOLUTION, 0.0, RESOLUTION, 0.0, 1.0)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
@@ -25,15 +52,14 @@ def showScreen():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
     iterate()
-    glColor3f(1.0, 0.0, 3.0)
-    square()
+    mesh()
     glutSwapBuffers()
 
 
 def main():
     glutInit()
     glutInitDisplayMode(GLUT_RGBA)
-    glutInitWindowSize(500, 500)
+    glutInitWindowSize(RESOLUTION, RESOLUTION)
     glutInitWindowPosition(0, 0)
     wind = glutCreateWindow("OpenGL Coding Practice")
     glutDisplayFunc(showScreen)
